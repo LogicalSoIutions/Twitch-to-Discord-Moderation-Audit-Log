33f87ad6735c8bfb04026e323061dcf82a55b6de
LogicalSoIutions <67069181+LogicalSoIutions@users.noreply.github.com>
2023-03-16 19:05:50 -0700

    refractor

--- bot.py
+++ bot.py
@@ -22,19 +22,8 @@ webhook_url = 'yourDiscordWebhookURLhere'
 # Heartbeat interval in seconds
 heartbeat_interval = 280
 
-# Connect to Twitch PubSub
-ws = websocket.WebSocket()
-ws.connect(endpoint)
-
-# Subscribe to channel point redemption topic
-subscribe_message = {
-    "type": "LISTEN",
-    "data": {"topics": [redemption_topic], "auth_token": token},
-}
-ws.send(json.dumps(subscribe_message))
-
 # Heartbeat function to send PING messages to Twitch at regular intervals
-def send_heartbeat():
+def send_heartbeat(ws):
     while True:
         ws.send(json.dumps({"type": "PING"}))
         print("PING")
@@ -241,65 +230,73 @@ def disable_r9k(data, webhook_url):
     response = requests.post(webhook_url, data=json.dumps(payload), headers=headers)
     response.raise_for_status()
 
-# Create and start heartbeat thread
-heartbeat_thread = threading.Thread(target=send_heartbeat)
-heartbeat_thread.start()
+subscribe_message = {
+            "type": "LISTEN",
+            "data": {"topics": [redemption_topic], "auth_token": token},
+        }
 
-# Listen for messages from Twitch PubSub
-while True:
-    try:
-        message = ws.recv()
-    except websocket.WebSocketConnectionClosedException:
-        # Handle connection loss by reconnecting and resubscribing
-        ws.close()
-        ws.connect(endpoint)
-        ws.send(json.dumps(subscribe_message))
-        continue
-    except KeyboardInterrupt:
-        print("Closed via CTRL + C")
-        sys.exit()
-    message = json.loads(message)
+def listen_for_messages(ws, webhook_url):
+    while True:
+        try:
+            message = ws.recv()
+        except websocket.WebSocketConnectionClosedException:
+            ws.connect(endpoint)
+            ws.send(json.dumps(subscribe_message))
+            continue
+        except KeyboardInterrupt:
+            print("Closed via CTRL + C")
+            sys.exit()
+        message = json.loads(message)
 
-    if message["type"] == "RECONNECT":
-        # Reconnect if Twitch requests it
-        ws.close()
-        ws.connect(endpoint)
-        ws.send(json.dumps(subscribe_message))
-    elif message["type"] == "MESSAGE":
-        data = json.loads(message["data"]["message"])
-        moderation_action = data["data"]["moderation_action"]
+        if message["type"] == "RECONNECT":
+            ws.connect(endpoint)
+            ws.send(json.dumps(subscribe_message))
+        elif message["type"] == "MESSAGE":
+            data = json.loads(message["data"]["message"])
+            moderation_action = data["data"]["moderation_action"]
 
-        if moderation_action == "delete":
-            delete_message(data, webhook_url)
-        elif moderation_action == "timeout":
-            timeout_message(data, webhook_url)
-        elif moderation_action == "slow":
-            enable_slowmode(data, webhook_url)
-        elif moderation_action == "emoteonly":
-            enable_emotemode(data, webhook_url)
-        elif moderation_action == "subscribers":
-            enable_submode(data, webhook_url)
-        elif moderation_action == "followers":
-            enable_followmode(data, webhook_url)
-        elif moderation_action == "r9kbeta":
-            enable_r9k(data, webhook_url)
-        elif moderation_action == "slowoff":
-            disable_slowmode(data, webhook_url)
-        elif moderation_action == "emoteonlyoff":
-            disable_emotemode(data, webhook_url)
-        elif moderation_action == "subscribersoff":
-            disable_submode(data, webhook_url)
-        elif moderation_action == "followersoff":
-            disable_followmode(data, webhook_url)
-        elif moderation_action == "r9kbetaoff":
-            disable_r9k(data, webhook_url)
-        elif moderation_action == "ban":
-            ban_message(data, webhook_url)
-        elif moderation_action == "unban":
-            unban_message(data, webhook_url)
-        elif moderation_action == "untimeout":
-            untimeout_message(data, webhook_url)
+            action_functions = {
+                "delete": delete_message,
+                "timeout": timeout_message,
+                "slow": enable_slowmode,
+                "emoteonly": enable_emotemode,
+                "subscribers": enable_submode,
+                "followers": enable_followmode,
+                "r9kbeta": enable_r9k,
+                "slowoff": disable_slowmode,
+                "emoteonlyoff": disable_emotemode,
+                "subscribersoff": disable_submode,
+                "followersoff": disable_followmode,
+                "r9kbetaoff": disable_r9k,
+                "ban": ban_message,
+                "unban": unban_message,
+                "untimeout": untimeout_message,
+            }
+
+            if moderation_action in action_functions:
+                action_functions[moderation_action](data, webhook_url)
+            else:
+                print(data)
         else:
-            print(data)
-    else:
-        print(message)
+            print(message)
+
+def main():
+    # Connect to Twitch PubSub
+        ws = websocket.WebSocket()
+        ws.connect(endpoint)
+        # Subscribe to channel point redemption topic
+        subscribe_message = {
+            "type": "LISTEN",
+            "data": {"topics": [redemption_topic], "auth_token": token},
+        }
+        ws.send(json.dumps(subscribe_message))
+
+        # Create and start heartbeat thread
+        heartbeat_thread = threading.Thread(target=send_heartbeat, args=(ws,))
+        heartbeat_thread.start()
+
+        # Listen for messages from Twitch PubSub
+        listen_for_messages(ws, webhook_url)
+
+if __name__ == "__main__":
+    main()
